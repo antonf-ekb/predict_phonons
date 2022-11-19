ead87559f0880c3160d84cb6ea0c13008703fdeb
Tatiana <tvpoveda@yandex.ru>
2022-11-19 22:49:53 +0500

    divided into functions

--- main.py
+++ main.py
@@ -2,57 +2,64 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import time
 from PIL import Image
-
 from matminer.featurizers.conversions import StrToComposition
 from matminer.featurizers.composition import ElementProperty
 from matminer.featurizers.conversions import CompositionToOxidComposition
 from matminer.featurizers.composition import OxidationStates
 
-#import models
-with open("dump/scaler", 'rb') as file:
-    scaler = pickle.load(file)
-with open("dump/model_kappa", 'rb') as file:
-    model_kappa = pickle.load(file)
-with open("dump/model_bulk", 'rb') as file:
-    model_bulk = pickle.load(file)
-with open("dump/model_shear", 'rb') as file:
-    model_shear = pickle.load(file)
+def calculate_values(cmpd):
+    # import models
+    with open("dump/scaler", 'rb') as file:
+        scaler = pickle.load(file)
+    with open("dump/model_kappa", 'rb') as file:
+        model_kappa = pickle.load(file)
+    with open("dump/model_bulk", 'rb') as file:
+        model_bulk = pickle.load(file)
+    with open("dump/model_shear", 'rb') as file:
+        model_shear = pickle.load(file)
 
-ep_feat = ElementProperty.from_preset(preset_name="magpie")
-os_feat = OxidationStates()
-
-
-# import Image from pillow to open images
-
-# Настройка боковой панели
-st.sidebar.title("About")
-img = Image.open("picture.png")
-st.sidebar.image(img, width=200)
-
-
-# cmpd="Mn2CoCrP2"
-st.title("Предсказание теплопроводности и упругих модулей кристаллических материалов")
-st.subheader("")
-cmpd = st.text_input("Введите химическую формулу соединения:",help="Введите без пробелов",placeholder="Mn2CoCrP2" )
-
-if (st.button("Предсказать")):
+    ep_feat = ElementProperty.from_preset(preset_name="magpie")
+    os_feat = OxidationStates()
     #make descriptor from the compound formula
     to_predict = pd.DataFrame(columns=["compound","nat_form","nelem"])
     try:
-        to_predict.loc[0,"compound"]=cmpd
+        to_predict.loc[0,"compound"] = cmpd
         to_predict = StrToComposition().featurize_dataframe(to_predict, "compound")
         to_predict = ep_feat.featurize_dataframe(to_predict, col_id="composition")
         to_predict = CompositionToOxidComposition().featurize_dataframe(to_predict, "composition")
         to_predict = os_feat.featurize_dataframe(to_predict, "composition_oxid")
-        to_predict.loc[0,"nelem"]=len(to_predict["composition"][0].as_dict().keys())
-        to_predict.loc[0,"nat_form"]=sum(to_predict["composition"][0].as_dict().values())
+        to_predict.loc[0,"nelem"] = len(to_predict["composition"][0].as_dict().keys())
+        to_predict.loc[0,"nat_form"] = sum(to_predict["composition"][0].as_dict().values())
         X_to_predict = scaler.transform(to_predict.drop(['composition','composition_oxid'], axis=1).set_index('compound').values)
         #make prediction
-        st.write("Решеточная теплопроводность: "+str(round(np.exp(model_kappa.predict(X_to_predict))[0],2)) + " Вт/(м*К)")
-        st.write("Модуль всестороннего сжатия: "+str(round(model_bulk.predict(X_to_predict)[0])) + " ГПа")
-        st.write("Модуль сдвига: "+str(round(model_shear.predict(X_to_predict)[0])) + " ГПа")
+        conductivity = round(np.exp(model_kappa.predict(X_to_predict))[0],2)
+        compression_modulest = round(model_bulk.predict(X_to_predict)[0])
+        shift_modulus = round(model_shear.predict(X_to_predict)[0])
+        return [conductivity, compression_modulest, shift_modulus]
     except:
-        st.error("Проверьте правильность ввода формулы!")
+        return [0]
 
+
+def main():
+    # Настройка боковой панели
+    st.sidebar.title("About")
+    # import Image from pillow to open images
+    #img = Image.open("picture.png")
+    #st.sidebar.image(img, width = 200)
+
+    # cmpd="Mn2CoCrP2"
+    st.title("Предсказание теплопроводности и упругих модулей кристаллических материалов")
+    st.subheader("")
+    cmpd = st.text_input("Введите химическую формулу соединения:",help="Введите формулу без пробелов",placeholder="Mn2CoCrP2" )
+    if (st.button("Предсказать")):
+        result = calculate_values(cmpd)
+        if len(result) == 1:
+            st.error("Проверьте правильность ввода формулы!")
+        else:
+            st.write("Решеточная теплопроводность: " + str(result[0]) + " Вт/(м*К)")
+            st.write("Модуль всестороннего сжатия: " + str(result[1]) + " ГПа")
+            st.write("Модуль сдвига: " + str(result[2]) + " ГПа")
+
+if __name__=="__main__":
+    main()
