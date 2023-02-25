75ba1a1a6bb4ba533133edd90b903095bde191b4
Татьяна <tvpoveda@yandex.ru>
2023-02-25 23:37:56 +0500

    Division by function

---
+++ test_main.py
@@ -0,0 +1,9 @@
+from main import calculate_values
+from main import get_models
+import pytest
+
+def test_calculate_values():
+    assert calculate_values("Mn2CoCrP2", get_models()) == [6.07, 163, 95]
+
+def test_calculate_values_bad():
+    assert calculate_values("", get_models()) == [0]
