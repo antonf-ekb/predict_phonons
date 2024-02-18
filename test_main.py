9d0c6bd8c194e88ce09f73116c6352767d059ad6
antonf-ekb <ngssoft@mail.ru>
2024-02-18 23:34:46 +0500

    Fixed some bugs in test_main.py

--- test_main.py
+++ test_main.py
@@ -4,10 +4,10 @@ from main import get_models
 
 def test_calculate_values():
     values = calculate_values("Mn2CoCrP2", get_models())
-    rezult = [6.07, 163, 95]
-    assert values[0]*0.5 <= rezult[0] <= values[0]*1.5
-    assert values[1] * 0.7 <= rezult[1] <= values[0] * 1.3
-    assert values[2] * 0.5 <= rezult[2] <= values[0] * 1.5
+    result = [6.07, 163, 95]
+    assert values[0]*0.5 <= result[0] <= values[0]*1.5
+    assert values[1] * 0.7 <= result[1] <= values[1] * 1.3
+    assert values[2] * 0.5 <= result[2] <= values[2] * 1.5
 
 
 def test_calculate_values_bad():
